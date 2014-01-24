5bed6d083e0f2933b55d75b0c4765567d1443afc
Harris Rafto <harrisrafto@BlowFish-rMBP-2.local>
2014-01-24 16:38:10 +0200

    commands.md translated

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,10 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(function()
-{
-	return getenv('ENV') ? : 'development';
-});
+$env = $app->detectEnvironment(array(
+
+        'local' => array('your-machine-name'),
+
+));
 
 /*
 |--------------------------------------------------------------------------
