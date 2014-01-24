6eb5d0d0068f4c5c6e45ffcfa89153208af20915
GD Ubuntu <george@drakakis.gr>
2014-01-24 22:23:52 +0200

    Homepage starter

---
+++ app/views/template.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel - Greek Fans Page</title>
+    {{ HTML::style('css/app.css') }}
+</head>
+<body>
+@yield('header')
+@yield('main')
+@yield('footer')
+</body>
+</html>
