00c2e56e8ba22b397ea20c1e8e4e6944ba380ec9
GD Ubuntu <george@drakakis.gr>
2014-01-25 12:25:03 +0200

    Fixed Foundation Setup

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -1,13 +1,28 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Laravel - Greek Fans Page</title>
-    {{ HTML::style('css/app.css') }}
-</head>
-<body>
-@yield('header')
-@yield('main')
-@yield('footer')
-</body>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{HTML::style('css/app.css')}}
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="row">
+          <div class="small-2 large-4 columns">.a.</div>
+          <div class="small-4 large-4 columns">.b.</div>
+          <div class="small-6 large-4 columns">.c.</div>
+        </div>
+        @yield('main')
+        @yield('footer')
+
+        @include('layout.analytics')
+    </body>
 </html>
