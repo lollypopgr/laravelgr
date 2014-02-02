97a4b8a957232644c7d353247a97a79e718f9b3c
GD Ubuntu <george@drakakis.gr>
2014-02-02 21:33:35 +0200

    Forum setup

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -6,4 +6,5 @@
     {{PictureFill::make("images/themes/fans.jpg","Greek Laravel Fans")}}
 
 <div class="community"><strong>{{$devscount}}</strong> προγραμματιστές ανακαλύφθηκαν. Ας γίνουμε περισσότεροι!</div>
+<div class="devthumbs">{{PictureFill::make("images/devs/devs.jpg","Greek Laravel Fans")}}</div>
 @stop
